642b8c88ba75ed959cc28ea54ae3016d2eb24345
Anurag Mishra <anurag2025mishra@gmail.com>
2023-04-14 23:55:05 +0530

    intro

---
+++ practice.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	// fmt.Print("Hello World")
+	fmt.Print("Hello\n")
+	fmt.Printf("Hello,my name is %s. It's April %d", "Anurag", 14)
+	//OutPut->
+	// Hello World
+	// Hello
+	// Hello,my name is Anurag. It's April 14
+}
